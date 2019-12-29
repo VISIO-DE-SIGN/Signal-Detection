7558fadd0f9c8ff6fe833921a81b215567ed4a75
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2019-12-30 00:20:30 +0100

    Charging image

---
+++ Detection.m
@@ -0,0 +1,10 @@
+% Signal detection
+
+%%
+% Charging image
+dataset_path = getenv('Dataset_path');
+image = strcat(dataset_path, "\camera00\00\image.000060.jp2");  %191 %60 %4766
+I = imread(image);
+
+%%
+

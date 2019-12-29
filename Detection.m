0c596f72c6566407fd812d2727ad5cc892cf985f
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2019-12-30 00:25:54 +0100

    Blue and red binarizing + corte bordes

--- Detection.m
+++ Detection.m
@@ -7,4 +7,18 @@ image = strcat(dataset_path, "\camera00\00\image.000060.jp2");  %191 %60 %4766
 I = imread(image);
 
 %%
+% Only blue and red pixels
+B = I(:,:,3) - I(:,:,1) - I(:,:,2);
+R = I(:,:,1) - I(:,:,2) - I(:,:,3);
+
+%%
+% Binarizing image
+blue = imbinarize(B,'adaptive');
+red = imbinarize(R,'adaptive');
+
+%%
+% recorte de franja de 2 pixeles
+blue = blue(3:end-2,3:end-2);
+red = red(3:end-2,3:end-2);
+
 

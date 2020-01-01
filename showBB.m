3493ece1dc7033c1d6e6c54510a1d09342db32dc
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2020-01-01 21:17:50 +0100

    Create showBB.m

---
+++ showBB.m
@@ -0,0 +1,17 @@
+function [] = showBB(image, regions, color)
+%filters Bounding boxes by their aspect ratio
+%   inputs: regions     -> BB to be shown
+%           image       -> base image
+%   output: NONE
+
+if nargin < 3
+   	color = 'blue';
+end
+  
+imshow(image);
+for i = 1:length(regions)
+    x = regions(i).BoundingBox(:,1);
+    y = regions(i).BoundingBox(:,2);
+    rectangle('Position',regions(i).BoundingBox,'EdgeColor',color);
+    text(x, y, num2str(regions(i).Area),'Color',color,'FontSize',14);
+end

f27c6258ccefaa5b8f00b91903f0431bcddb913d
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2020-01-05 00:48:43 +0100

    changing BB to struct (2)

--- filter_by_area.m
+++ filter_by_area.m
@@ -1,16 +1,26 @@
-function [index] = filter_by_area(regions,area)
+function [index] = filter_by_area(regions,min_area,BB_format)
 %filters Bounding boxes by their aspect ratio
 %   inputs: regions     -> BB to be filtered
-%           area        -> minimum area
+%           min_area    -> minimum area
 %   output: index       -> retuns the index of BBs that meet the condition
 
 index = [];
 cont = 1;
 
-for i = 1:length(regions)
-    if(regions(i).Area >= area)
-        index(cont) = i;
-        cont = cont+1;
+if BB_format
+    for i = 1:length(regions)
+        area = regions(i).width * regions(i).height;
+        if(area >= min_area)
+            index(cont) = i;
+            cont = cont+1;
+        end
     end
+else
+    for i = 1:length(regions)
+        if(regions(i).Area >= min_area)
+            index(cont) = i;
+            cont = cont+1;
+        end
 
+    end
 end

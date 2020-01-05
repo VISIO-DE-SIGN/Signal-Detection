7d13422cb5b87cc8924076ea61a015d2a7a0698a
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2020-01-05 01:04:43 +0100

    changing BB to struct (3)

--- showBB.m
+++ showBB.m
@@ -1,4 +1,4 @@
-function [] = showBB(image, regions, color, hold)
+function [] = showBB(image, regions, color, hold, BB_format)
 %filters Bounding boxes by their aspect ratio
 %   inputs: regions     -> BB to be shown
 %           image       -> base image
@@ -6,6 +6,7 @@ function [] = showBB(image, regions, color, hold)
 
 if nargin < 3
    	color = 'blue';
+    BB_format = false;
 end
 
 if (hold)
@@ -13,9 +14,22 @@ if (hold)
     imshow(image);
 end
 
-for i = 1:length(regions)
-    x = regions(i).BoundingBox(:,1);
-    y = regions(i).BoundingBox(:,2);
-    rectangle('Position',regions(i).BoundingBox,'EdgeColor',color);
-    text(x, y, num2str(regions(i).Area),'Color',color,'FontSize',14);
+if BB_format
+    for i = 1:length(regions)
+        x = regions(i).x;
+        y = regions(i).y;
+        w = regions(i).width;
+        h = regions(i).height;
+        bb = [x,y,w,h];
+        area = w*h;
+        rectangle('Position',bb,'EdgeColor',color);
+        text(x, y, num2str(area),'Color',color,'FontSize',12);
+    end
+else
+    for i = 1:length(regions)
+        x = regions(i).BoundingBox(:,1);
+        y = regions(i).BoundingBox(:,2);
+        rectangle('Position',regions(i).BoundingBox,'EdgeColor',color);
+        text(x, y, num2str(regions(i).Area),'Color',color,'FontSize',14);
+    end
 end

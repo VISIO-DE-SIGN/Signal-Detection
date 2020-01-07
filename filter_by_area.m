b77a66fd6f8b63ef564950c3ce3c46b7aadbb7c9
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2020-01-07 20:45:03 +0100

    area filtering with upper and lower bounds

--- filter_by_area.m
+++ filter_by_area.m
@@ -1,7 +1,7 @@
-function [index] = filter_by_area(regions,min_area,BB_format)
+function [index] = filter_by_area(regions,area,BB_format)
 %filters Bounding boxes by their aspect ratio
 %   inputs: regions     -> BB to be filtered
-%           min_area    -> minimum area
+%           area []     -> minimum and maximm area
 %   output: index       -> retuns the index of BBs that meet the condition
 
 if nargin < 3
@@ -11,17 +11,20 @@ end
 index = [];
 cont = 1;
 
+min_area = area(1);
+max_area = area(2);
+
 if BB_format
     for i = 1:length(regions)
         area = regions(i).width * regions(i).height;
-        if(area >= min_area)
+        if(area >= min_area && area <= max_area)
             index(cont) = i;
             cont = cont+1;
         end
     end
 else
     for i = 1:length(regions)
-        if(regions(i).Area >= min_area)
+        if(regions(i).Area >= min_area && regions(i).Area <= max_area)
             index(cont) = i;
             cont = cont+1;
         end
